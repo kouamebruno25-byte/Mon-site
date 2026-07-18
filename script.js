2c05f3076e5953f3260f8cfede413b49b04cbe15
Kouame-Bruno <kouamebruno25@gmail.com>
2026-07-18 23:13:01 +0000

    Ajout de logique JS au bouton

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.cta-button').addEventListener('click', function(e) {
+    // Si tu veux empêcher le saut vers la section contact et juste afficher une alerte :
+    // e.preventDefault();
+    alert("Bienvenue ! Nous allons lancer votre projet.");
+});
+
