183c2b22a5db12bdbf88658f3b7ec57767f20e73
Kouame-Bruno <kouamebruno25@gmail.com>
2026-07-19 01:05:14 +0000

    Mise à jour finale des fichiers

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-// On sélectionne tous les éléments cibles
-const elements = document.querySelectorAll('.nav-links a, .cta-button');
-
-elements.forEach(el => {
-    el.addEventListener('click', function() {
-        // Ajoute la classe CSS pour l'animation
-        this.classList.add('clique-effet');
-        
-        // Retire la classe après 200ms pour que l'effet disparaisse
-        setTimeout(() => {
-            this.classList.remove('clique-effet');
-        }, 200);
-    });
-});
-
 const btnTop = document.getElementById('btn-top');
 
-// Afficher/Cacher le bouton selon le scroll
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
         btnTop.style.display = "block";
@@ -24,11 +8,7 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// Action au clic : remonter en haut
 btnTop.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
